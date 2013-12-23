ef2668483af5f6bc6361f27bc0a364b0a78a22a5
Kord Campbell <kordless@stackgeek.com>
2013-12-22 21:04:56 -0800

    updated with formatting

--- modules/calc.py
+++ modules/calc.py
@@ -9,15 +9,21 @@ import math
 def calc(bot, trigger):
     # price
     import btceapi
-    connection = btceapi.BTCEConnection()
-    ticker = btceapi.getTicker("ltc_usd", connection)
-    avg = getattr(ticker, "avg")
+    try:
+        connection = btceapi.BTCEConnection()
+        ticker = btceapi.getTicker("ltc_usd", connection)
+        last = getattr(ticker, "last")
+        avg = getattr(ticker, "avg")
 
-    # diff and block count
-    content = urllib2.urlopen("http://litecoinscout.com/chain/Litecoin/q/nethash/1/-1")
-    data = content.readlines()[-1].split(",")
-    diff = float(data[4])
-    blockscount = float(data[0])
+        # diff and block count
+        content = urllib2.urlopen("http://litecoinscout.com/chain/Litecoin/q/nethash/1/-1")
+        data = content.readlines()[-1].split(",")
+        diff = float(data[4])
+        blockscount = float(data[0])
+    except:
+        say_string = "Either litecoinscount.com or BTC-e is ignoring my requests. Wait a few and try again."
+        bot.say(say_string)
+        return
 
     # grab intended hash rate
     parms = trigger.group(2)
@@ -42,12 +48,13 @@ def calc(bot, trigger):
     revenue = secsinyear / time_per_block * coinsperblock
 
     # clean up and convert
-    yrrevenue = int(revenue)
-    yrinusdollars = revenue * int(avg)
+    yrrevenue = revenue
+    yrinusdollars = int(revenue) * last
 
     # print
-    say_string = "Calculating return with %sKH/s with %s difficulty: US$%s/yr (%s LTC) | US$%s/month (%s LTC) | US$%s/week (%s LTC) | US$%s/day (%s LTC)" % (hashrate/1000, diff, int(yrinusdollars), yrrevenue, int(yrinusdollars/12), yrrevenue/12, int(yrinusdollars/52), yrrevenue/52, int(yrinusdollars/365), yrrevenue/365)
+    say_string = "Calculating return with %sKH/s with %s difficulty using last trade of LTC: US$%s | US$%s/yr (%s LTC) | US$%s/month (%s LTC) | US$%s/week (%s LTC) | US$%s/day (%s LTC) | This calculation brought to you by kordless: BF4 promo codes for 2 LTC: http://goo.gl/zRrmTT" % (hashrate/1000, diff, round(last, 2), round(yrinusdollars,2), round(yrrevenue,4), round(yrinusdollars/12,2), round(yrrevenue/12,4), round(yrinusdollars/52,2), round(yrrevenue/52,4), round(yrinusdollars/365,2), round(yrrevenue/365,4))
 
     if hashrate/1000 > 100000:
         say_string = "Mules gives %s a blank stare." % trigger.nick
+
     bot.say(say_string)
